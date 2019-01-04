bdaa558bc7167a4c541c0e684ab7408fdced1be5
qicosmos <qicosmos@163.com>
2019-01-04 16:27:15 +0800

    remove has_before/has_after macro, using is_detective instead

--- utils.hpp
+++ utils.hpp
@@ -411,19 +411,47 @@ namespace cinatra {
 		}
 	}
 
-#define HAS_MEMBER(member)\
-template<typename T, typename... Args>\
-struct has_##member\
-{\
-private:\
-    template<typename U> static auto Check(int) -> decltype(std::declval<U>().member(std::declval<Args>()...), std::true_type()); \
-	template<typename U> static std::false_type Check(...);\
-public:\
-	enum{value = std::is_same<decltype(Check<T>(0)), std::true_type>::value};\
-};
+	//for is_detective
+	namespace {
+		struct nonesuch {
+			nonesuch() = delete;
+			~nonesuch() = delete;
+			nonesuch(const nonesuch&) = delete;
+			void operator=(const nonesuch&) = delete;
+		};
 
-	HAS_MEMBER(before)
-	HAS_MEMBER(after)
+		template<class Default, class AlwaysVoid,
+			template<class...> class Op, class... Args>
+		struct detector {
+			using value_t = std::false_type;
+			using type = Default;
+		};
+
+
+		template<class Default, template<class...> class Op, class... Args>
+		struct detector<Default, std::void_t<Op<Args...>>, Op, Args...> {
+			using value_t = std::true_type;
+			using type = Op<Args...>;
+		};
+
+		template<template<class...> class Op, class... Args>
+		using is_detected = typename detector<nonesuch, void, Op, Args...>::value_t;
+
+		template<template<class...> class Op, class... Args>
+		using detected_t = typename detector<nonesuch, void, Op, Args...>::type;
+
+		template<class T, typename... Args>
+		using has_before_t = decltype(std::declval<T>().before(std::declval<Args>()...));
+
+		template<class T, typename... Args>
+		using has_after_t = decltype(std::declval<T>().after(std::declval<Args>()...));
+	}
+	
+	template<typename T, typename... Args>
+	using has_before = is_detected<has_before_t, T, Args...>;
+
+	template<typename T, typename... Args>
+	using has_after = is_detected<has_after_t, T, Args...>;
 
 	template <typename... Args, typename F, std::size_t... Idx>
 	constexpr void for_each_l(std::tuple<Args...>& t, F&& f, std::index_sequence<Idx...>) {
