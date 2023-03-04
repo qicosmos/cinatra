100069f8ec57831190d9685e35cf9bcd8a0ec8a8
ptbxzrt <ptbxzrt@aliyun.com>
2023-03-04 17:12:36 +0800

    add test coverage

--- cmake/dependency.cmake
+++ cmake/dependency.cmake
@@ -1,2 +1,12 @@
 # 3rd-party package load
 add_subdirectory(${cinatra_SOURCE_DIR}/thirdparty)
+
+# coverage test
+option(COVERAGE_TEST "Build with unit test coverage" OFF)
+if(COVERAGE_TEST)
+    if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fprofile-arcs -ftest-coverage --coverage")
+    else()
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fprofile-instr-generate -fcoverage-mapping")
+    endif()
+endif()
