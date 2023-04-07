d086f2b8c85698f56ef29d31c9071a384eec1361
qicosmos <qicosmos@163.com>
2023-04-07 20:39:01 +0800

    begin to add options

--- press_tool/main.cpp
+++ press_tool/main.cpp
@@ -1,15 +1,28 @@
-#include "../include/cinatra.hpp"
-#include "async_simple/Try.h"
-#include "config.h"
-#include "cmdline.h"
-#include "utils.h"
-
 #include <iostream>
 #include <memory>
-#include <vector>
 #include <thread>
+#include <vector>
 
-int main(int argc,  char* argv[])
-{
-    return 0;
+#include "../include/cinatra.hpp"
+#include "async_simple/Try.h"
+#include "cmdline.h"
+#include "config.h"
+#include "utils.h"
+
+int main(int argc, char* argv[]) {
+  cmdline::parser a;
+  a.add<int>("connections", 'c',
+             "total number of HTTP connections to keep open with"
+             "                   each thread handling N = connections/threads",
+             true, 0);
+  a.add<std::string>("duration", 'd', "duration of the test, e.g. 2s, 2m, 2h",
+                     false, "15s");
+  a.add<int>("threads", 't', "total number of threads to use", false, 1);
+  a.add<std::string>("header", 'H',
+                     "HTTP header to add to request, e.g. \"User-Agent: wrk\"",
+                     false, "");
+
+  a.parse_check(argc, argv);
+
+  return 0;
 }
