10e5a7f744632d68810b6dbea971f53162a7660c
helintongh <agh6399@gmail.com>
2023-04-07 18:21:29 +0800

    feature: coro http press initialization

---
+++ press_tool/main.cpp
@@ -0,0 +1,15 @@
+#include "../include/cinatra.hpp"
+#include "async_simple/Try.h"
+#include "config.h"
+#include "cmdline.h"
+#include "utils.h"
+
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <thread>
+
+int main(int argc,  char* argv[])
+{
+    return 0;
+}
