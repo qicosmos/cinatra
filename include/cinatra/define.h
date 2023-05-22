76cd68ec0965e1cb8516994d647dac7568ff008d
qicosmos <qicosmos@163.com>
2023-05-22 17:58:39 +0800

    Improve timestamp (#318)

--- include/cinatra/define.h
+++ include/cinatra/define.h
@@ -80,6 +80,7 @@ inline std::unordered_map<std::string, std::string> g_content_type_map = {
 struct NonSSL {};
 struct SSL {};
 
+namespace time_util {
 /*
   IMF-fixdate = day-name "," SP date1 SP time-of-day SP GMT
   day-name     = %s"Mon" / %s"Tue" / %s"Wed"
@@ -116,7 +117,14 @@ enum component_of_time_format {
   comma,
   colon
 };
-inline std::vector<component_of_time_format> http_time_format{
+
+inline constexpr std::array<int, 17> month_table = {
+    11, 4, -1, 7, -1, -1, -1, 0, 6, 3, 5, 2, 10, 8, -1, 9, 1};
+
+inline constexpr std::array<int, 17> week_table = {
+    2, 4, 3, 1, -1, -1, -1, 6, -1, -1, -1, -1, 0, -1, -1, 5, -1};
+
+inline constexpr std::array<component_of_time_format, 16> http_time_format{
     component_of_time_format::day_name, component_of_time_format::comma,
     component_of_time_format::SP,       component_of_time_format::day,
     component_of_time_format::SP,       component_of_time_format::month,
@@ -125,30 +133,24 @@ inline std::vector<component_of_time_format> http_time_format{
     component_of_time_format::colon,    component_of_time_format::minute,
     component_of_time_format::colon,    component_of_time_format::second,
     component_of_time_format::SP,       component_of_time_format::GMT};
-const inline int len_of_http_format =
+constexpr inline int len_of_http_format =
     3 + 1 + 1 + 2 + 1 + 3 + 1 + 4 + 1 + 2 + 1 + 2 + 1 + 2 + 1 + 3;
-inline std::unordered_map<std::string_view, int> name_of_day = {
-    {"Sun", 0}, {"Mon", 1}, {"Tue", 2}, {"Wed", 3},
-    {"Thu", 4}, {"Fri", 5}, {"Sat", 6}};
-inline std::unordered_map<std::string_view, int> name_of_month = {
-    {"Jan", 0}, {"Feb", 1}, {"Mar", 2}, {"Apr", 3}, {"May", 4},  {"Jun", 5},
-    {"Jul", 6}, {"Aug", 7}, {"Sep", 8}, {"Oct", 9}, {"Nov", 10}, {"Dec", 11}};
-const inline std::int64_t absolute_zero_year = -292277022399;
-const inline std::int64_t days_per_100_years = 365 * 100 + 24;
-const inline std::int64_t days_per_400_years = 365 * 400 + 97;
-const inline std::int64_t days_per_4_years = 365 * 4 + 1;
-const inline std::int64_t seconds_per_minute = 60;
-const inline std::int64_t seconds_per_hour = 60 * seconds_per_minute;
-const inline std::int64_t seconds_per_day = 24 * seconds_per_hour;
-const inline std::int64_t seconds_per_week = 7 * seconds_per_day;
-const inline std::int64_t internal_year = 1;
-const inline std::int64_t absolute_to_internal =
+constexpr inline std::int64_t absolute_zero_year = -292277022399;
+constexpr inline std::int64_t days_per_100_years = 365 * 100 + 24;
+constexpr inline std::int64_t days_per_400_years = 365 * 400 + 97;
+constexpr inline std::int64_t days_per_4_years = 365 * 4 + 1;
+constexpr inline std::int64_t seconds_per_minute = 60;
+constexpr inline std::int64_t seconds_per_hour = 60 * seconds_per_minute;
+constexpr inline std::int64_t seconds_per_day = 24 * seconds_per_hour;
+constexpr inline std::int64_t seconds_per_week = 7 * seconds_per_day;
+constexpr inline std::int64_t internal_year = 1;
+constexpr inline std::int64_t absolute_to_internal =
     (absolute_zero_year - internal_year) *
     std::int64_t(365.2425 * seconds_per_day);
-const inline std::int64_t unix_to_internal =
+constexpr inline std::int64_t unix_to_internal =
     (1969 * 365 + 1969 / 4 - 1969 / 100 + 1969 / 400) * seconds_per_day;
-const inline std::int64_t internal_to_unix = -unix_to_internal;
-const inline std::vector<std::int32_t> days_before = {
+constexpr inline std::int64_t internal_to_unix = -unix_to_internal;
+constexpr inline std::array<std::int32_t, 13> days_before = {
     0,
     31,
     31 + 28,
@@ -163,4 +165,5 @@ const inline std::vector<std::int32_t> days_before = {
     31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30,
     31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 + 31,
 };
+}  // namespace time_util
 }  // namespace cinatra
