525c99ed47cf3fac7e1d79900c69d02a313f8fa4
qicosmos <qicosmos@163.com>
2023-08-26 19:17:12 +0800

    update doc

--- include/cinatra/ylt/coro_io/io_context_pool.hpp
+++ include/cinatra/ylt/coro_io/io_context_pool.hpp
@@ -252,13 +252,15 @@ inline T &g_block_io_context_pool(
 }
 
 template <typename T = io_context_pool>
-inline auto get_global_executor() {
-  return g_io_context_pool<T>().get_executor();
+inline auto get_global_executor(
+    unsigned pool_size = std::thread::hardware_concurrency()) {
+  return g_io_context_pool<T>().get_executor(pool_size);
 }
 
 template <typename T = io_context_pool>
-inline auto get_global_block_executor() {
-  return g_block_io_context_pool<T>().get_executor();
+inline auto get_global_block_executor(
+    unsigned pool_size = std::thread::hardware_concurrency()) {
+  return g_block_io_context_pool<T>().get_executor(pool_size);
 }
 
 }  // namespace coro_io
