2cb7c732e18ed4fd591aa30fb3c52cce58acb952
qicosmos <qicosmos@163.com>
2023-09-05 17:33:31 +0800

    server start stop

---
+++ include/cinatra/coro_http_server.hpp
@@ -0,0 +1,145 @@
+#pragma once
+
+#include <asio/dispatch.hpp>
+
+#include "asio/streambuf.hpp"
+#include "async_simple/Promise.h"
+#include "async_simple/coro/Lazy.h"
+#include "cinatra_log_wrapper.hpp"
+#include "ylt/coro_io/coro_io.hpp"
+#include "ylt/coro_io/io_context_pool.hpp"
+
+namespace cinatra {
+class coro_http_server {
+ public:
+  coro_http_server(size_t thread_num, unsigned short port)
+      : pool_(thread_num),
+        port_(port),
+        acceptor_(pool_.get_executor()->get_asio_executor()) {}
+
+  ~coro_http_server() {
+    CINATRA_LOG_INFO << "coro_rpc_server will quit";
+    stop();
+  }
+
+  // only call once, not thread safe.
+  std::errc sync_start() noexcept {
+    auto ret = async_start();
+    ret.wait();
+    return ret.value();
+  }
+
+  // only call once, not thread safe.
+  async_simple::Future<std::errc> async_start() {
+    auto ec = listen();
+
+    async_simple::Promise<std::errc> promise;
+    auto future = promise.getFuture();
+
+    if (ec == std::errc{}) {
+      thd_ = std::thread([this] {
+        pool_.run();
+      });
+
+      accept().start([p = std::move(promise)](auto &&res) mutable {
+        if (res.hasError()) {
+          p.setValue(std::errc::io_error);
+        }
+        else {
+          p.setValue(res.value());
+        }
+      });
+    }
+    else {
+      promise.setValue(ec);
+    }
+
+    return std::move(future);
+  }
+
+  // only call once, not thread safe.
+  void stop() {
+    if (!thd_.joinable()) {
+      return;
+    }
+    close_acceptor();
+    CINATRA_LOG_INFO << "wait for server's thread-pool finish all work.";
+    pool_.stop();
+    CINATRA_LOG_INFO << "server's thread-pool finished.";
+    thd_.join();
+    CINATRA_LOG_INFO << "stop coro_rpc_server ok";
+  }
+
+  // call it after server async_start or sync_start.
+  uint16_t port() const { return port_; }
+
+ private:
+  std::errc listen() {
+    CINATRA_LOG_INFO << "begin to listen";
+    using asio::ip::tcp;
+    auto endpoint = tcp::endpoint(tcp::v4(), port_);
+    acceptor_.open(endpoint.protocol());
+#ifdef __GNUC__
+    acceptor_.set_option(tcp::acceptor::reuse_address(true));
+#endif
+    asio::error_code ec;
+    acceptor_.bind(endpoint, ec);
+    if (ec) {
+      CINATRA_LOG_ERROR << "bind port: " << port_ << " error: " << ec.message();
+      acceptor_.cancel(ec);
+      acceptor_.close(ec);
+      return std::errc::address_in_use;
+    }
+#ifdef _MSC_VER
+    acceptor_.set_option(tcp::acceptor::reuse_address(true));
+#endif
+    acceptor_.listen();
+
+    auto end_point = acceptor_.local_endpoint(ec);
+    if (ec) {
+      CINATRA_LOG_ERROR << "get local endpoint port: " << port_
+                        << " error: " << ec.message();
+      return std::errc::address_in_use;
+    }
+    port_ = end_point.port();
+
+    CINATRA_LOG_INFO << "listen port " << port_ << " successfully";
+    return {};
+  }
+
+  async_simple::coro::Lazy<std::errc> accept() {
+    for (;;) {
+      auto executor = pool_.get_executor();
+      asio::ip::tcp::socket socket(executor->get_asio_executor());
+      auto error = co_await coro_io::async_accept(acceptor_, socket);
+      if (error) {
+        CINATRA_LOG_INFO << "accept failed, error: " << error.message();
+        if (error == asio::error::operation_aborted ||
+            error == asio::error::bad_descriptor) {
+          acceptor_close_waiter_.set_value();
+          co_return std::errc::operation_canceled;
+        }
+        continue;
+      }
+
+      CINATRA_LOG_DEBUG << "new connection comming";
+    }
+  }
+
+  void close_acceptor() {
+    asio::dispatch(acceptor_.get_executor(), [this]() {
+      asio::error_code ec;
+      acceptor_.cancel(ec);
+      acceptor_.close(ec);
+    });
+    acceptor_close_waiter_.get_future().wait();
+  }
+
+ private:
+  coro_io::io_context_pool pool_;
+  uint16_t port_;
+  asio::ip::tcp::acceptor acceptor_;
+  std::thread thd_;
+  std::promise<void> acceptor_close_waiter_;
+};
+}  // namespace cinatra
---
+++ tests/test_coro_http_server.cpp
@@ -0,0 +1,48 @@
+#include <chrono>
+#include <future>
+#include <system_error>
+#include <thread>
+#define DOCTEST_CONFIG_IMPLEMENT
+#include <iostream>
+
+#include "cinatra/coro_http_server.hpp"
+#include "doctest/doctest.h"
+
+TEST_CASE("test listen random port") {
+  cinatra::coro_http_server server(1, 0);
+  server.async_start();
+  CHECK(server.port() > 0);
+}
+
+TEST_CASE("test server start and stop") {
+  cinatra::coro_http_server server(1, 9000);
+  auto future = server.async_start();
+
+  cinatra::coro_http_server server2(1, 9000);
+  auto future2 = server2.async_start();
+  future2.wait();
+  auto ec = future2.value();
+  CHECK(ec == std::errc::address_in_use);
+}
+
+TEST_CASE("test server sync_start and stop") {
+  cinatra::coro_http_server server(1, 0);
+
+  std::promise<void> promise;
+  std::errc ec;
+  std::thread thd([&] {
+    promise.set_value();
+    ec = server.sync_start();
+  });
+  promise.get_future().wait();
+  std::this_thread::sleep_for(std::chrono::milliseconds(200));
+  server.stop();
+  thd.join();
+  CHECK(server.port() > 0);
+  CHECK(ec == std::errc::operation_canceled);
+}
+
+DOCTEST_MSVC_SUPPRESS_WARNING_WITH_PUSH(4007) int main(int argc, char **argv) {
+  return doctest::Context(argc, argv).run();
+}
+DOCTEST_MSVC_SUPPRESS_WARNING_POP
