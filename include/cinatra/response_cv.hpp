fe24921e536aedb2b4d1471f60cdc1479c9fb6af
qicosmos <qicosmos@163.com>
2024-01-11 22:23:28 +0800

    more status

--- include/cinatra/response_cv.hpp
+++ include/cinatra/response_cv.hpp
@@ -73,36 +73,93 @@ enum class content_encoding { gzip, none };
 
 // http response status string
 namespace http_status_string {
+inline constexpr std::string_view http_continue = "HTTP/1.1 100 Continue\r\n";
 inline constexpr std::string_view switching_protocols =
     "HTTP/1.1 101 Switching Protocals\r\n";
+inline constexpr std::string_view processing = "HTTP/1.1 102 Processing\r\n";
 inline constexpr std::string_view rep_ok = "HTTP/1.1 200 OK\r\n";
 inline constexpr std::string_view rep_created = "HTTP/1.1 201 Created\r\n";
 inline constexpr std::string_view rep_accepted = "HTTP/1.1 202 Accepted\r\n";
+inline constexpr std::string_view rep_nonauthoritative =
+    "HTTP/1.1 203 Nonauthoritative\r\n";
 inline constexpr std::string_view rep_no_content =
     "HTTP/1.1 204 No Content\r\n";
+inline constexpr std::string_view rep_reset_content =
+    "HTTP/1.1 205 Reset Content\r\n";
 inline constexpr std::string_view rep_partial_content =
     "HTTP/1.1 206 Partial Content\r\n";
+inline constexpr std::string_view rep_multi_status =
+    "HTTP/1.1 207 Multi Status\r\n";
+inline constexpr std::string_view rep_already_reported =
+    "HTTP/1.1 208 Already Reported\r\n";
+inline constexpr std::string_view rep_im_used = "HTTP/1.1 226 Im Used\r\n";
 inline constexpr std::string_view rep_multiple_choices =
     "HTTP/1.1 300 Multiple Choices\r\n";
 inline constexpr std::string_view rep_moved_permanently =
     "HTTP/1.1 301 Moved Permanently\r\n";
-inline constexpr std::string_view rep_temporary_redirect =
-    "HTTP/1.1 307 Temporary Redirect\r\n";
 inline constexpr std::string_view rep_moved_temporarily =
     "HTTP/1.1 302 Moved Temporarily\r\n";
 inline constexpr std::string_view rep_not_modified =
     "HTTP/1.1 304 Not Modified\r\n";
+inline constexpr std::string_view rep_use_proxy = "HTTP/1.1 305 Use Proxy\r\n";
+inline constexpr std::string_view rep_temporary_redirect =
+    "HTTP/1.1 307 Temporary Redirect\r\n";
+inline constexpr std::string_view rep_permanent_redirect =
+    "HTTP/1.1 308 Permanent Redirect\r\n";
 inline constexpr std::string_view rep_bad_request =
     "HTTP/1.1 400 Bad Request\r\n";
 inline constexpr std::string_view rep_unauthorized =
     "HTTP/1.1 401 Unauthorized\r\n";
+inline constexpr std::string_view rep_payment_required =
+    "HTTP/1.1 402 Payment Required\r\n";
 inline constexpr std::string_view rep_forbidden = "HTTP/1.1 403 Forbidden\r\n";
 inline constexpr std::string_view rep_not_found = "HTTP/1.1 404 Not Found\r\n";
 inline constexpr std::string_view rep_method_not_allowed =
     "HTTP/1.1 405 Method Not Allowed\r\n";
+inline constexpr std::string_view rep_not_acceptable =
+    "HTTP/1.1 406 Not Acceptable\r\n";
+inline constexpr std::string_view rep_proxy_authentication_required =
+    "HTTP/1.1 407 Proxy Authentication Required\r\n";
+inline constexpr std::string_view rep_request_timeout =
+    "HTTP/1.1 408 Request Timeout\r\n";
 inline constexpr std::string_view rep_conflict = "HTTP/1.1 409 Conflict\r\n";
+inline constexpr std::string_view rep_gone = "HTTP/1.1 410 Gone\r\n";
+inline constexpr std::string_view rep_length_required =
+    "HTTP/1.1 411 Length Required\r\n";
+inline constexpr std::string_view rep_precondition_failed =
+    "HTTP/1.1 412 Precondition Failed\r\n";
+inline constexpr std::string_view rep_request_entity_too_large =
+    "HTTP/1.1 413 Request Entity Too Large\r\n";
+inline constexpr std::string_view rep_request_uri_too_long =
+    "HTTP/1.1 414 Request Uri Too Long\r\n";
+inline constexpr std::string_view rep_unsupported_media_type =
+    "HTTP/1.1 415 Unsupported Media Type\r\n";
 inline constexpr std::string_view rep_range_not_satisfiable =
     "HTTP/1.1 416 Requested Range Not Satisfiable\r\n";
+inline constexpr std::string_view rep_expectation_failed =
+    "HTTP/1.1 417 Expectation Failed\r\n";
+inline constexpr std::string_view rep_im_a_teapot =
+    "HTTP/1.1 418 Im a Teapot\r\n";
+inline constexpr std::string_view rep_enchance_your_calm =
+    "HTTP/1.1 420 Enchance Your Calm\r\n";
+inline constexpr std::string_view rep_misdirected_request =
+    "HTTP/1.1 421 Misdirected Request\r\n";
+inline constexpr std::string_view rep_unprocessable_entity =
+    "HTTP/1.1 422 Unprocessable Entity\r\n";
+inline constexpr std::string_view rep_locked = "HTTP/1.1 423 Locked\r\n";
+inline constexpr std::string_view rep_failed_dependency =
+    "HTTP/1.1 424 Failed_Dependency\r\n";
+inline constexpr std::string_view rep_too_early = "HTTP/1.1 425 Too Early\r\n";
+inline constexpr std::string_view rep_upgrade_required =
+    "HTTP/1.1 426 Upgrade Required\r\n";
+inline constexpr std::string_view rep_precondition_required =
+    "HTTP/1.1 428 Precondition Required\r\n";
+inline constexpr std::string_view rep_too_many_requests =
+    "HTTP/1.1 429 Too Many Requests\r\n";
+inline constexpr std::string_view rep_request_header_fields_too_large =
+    "HTTP/1.1 431 Request Header Fields Too Large\r\n";
+inline constexpr std::string_view rep_unavailable_for_legal_reasons =
+    "HTTP/1.1 451 Unavailabl For Legal Reasons\r\n";
 inline constexpr std::string_view rep_internal_server_error =
     "HTTP/1.1 500 Internal Server Error\r\n";
 inline constexpr std::string_view rep_not_implemented =
@@ -111,6 +168,20 @@ inline constexpr std::string_view rep_bad_gateway =
     "HTTP/1.1 502 Bad Gateway\r\n";
 inline constexpr std::string_view rep_service_unavailable =
     "HTTP/1.1 503 Service Unavailable\r\n";
+inline constexpr std::string_view rep_gateway_timeout =
+    "HTTP/1.1 504 Gateway Timeout\r\n";
+inline constexpr std::string_view rep_version_not_supported =
+    "HTTP/1.1 505 Version Not Supported\r\n";
+inline constexpr std::string_view rep_variant_also_negotiates =
+    "HTTP/1.1 506 Variant Also Negotiates\r\n";
+inline constexpr std::string_view rep_insufficient_storage =
+    "HTTP/1.1 507 Insufficient Storage\r\n";
+inline constexpr std::string_view rep_loop_detected =
+    "HTTP/1.1 508 Loop Detected\r\n";
+inline constexpr std::string_view rep_not_extended =
+    "HTTP/1.1 510 Not Extended\r\n";
+inline constexpr std::string_view rep_network_authentication_required =
+    "HTTP/1.1 511 Network Authentication Required\r\n";
 }  // namespace http_status_string
 
 inline constexpr std::string_view rep_html =
