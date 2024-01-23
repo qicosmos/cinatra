79509a40cc8762965e32e8ee0f6e502d3aa2f0d6
qicosmos <qicosmos@163.com>
2024-01-23 19:58:51 +0800

    Update benchmark url

--- example/benchmark.cpp
+++ example/benchmark.cpp
@@ -6,7 +6,7 @@ using namespace std::chrono_literals;
 int main() {
   coro_http_server server(std::thread::hardware_concurrency(), 8090);
   server.set_http_handler<GET>(
-      "/", [](coro_http_request& req, coro_http_response& resp) {
+      "/plaintext", [](coro_http_request& req, coro_http_response& resp) {
         resp.need_date_head(false);
         resp.set_status_and_content(status_type::ok, "Hello, world!");
       });
