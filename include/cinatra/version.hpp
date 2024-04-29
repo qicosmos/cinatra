ab6faafb440eaaa707744eae22de72b4ec13e1d3
qicosmos <qicosmos@163.com>
2024-04-29 17:45:30 +0800

    add version

---
+++ include/cinatra/version.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+// Note: Update the version when release a new version.
+
+// CINATRA_VERSION % 100 is the sub-minor version
+// CINATRA_VERSION / 100 % 1000 is the minor version
+// CINATRA_VERSION / 100000 is the major version
+#define CINATRA_VERSION 901  // 0.9.1
