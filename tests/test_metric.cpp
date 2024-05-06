08829913e6402d8b179a4b09dd510289f09a3fc3
qicosmos <qicosmos@163.com>
2024-05-06 11:52:00 +0800

    test guage

--- tests/test_metric.cpp
+++ tests/test_metric.cpp
@@ -1,4 +1,4 @@
-#include <stdexcept>
+#include "cinatra/metric/guage.hpp"
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "cinatra/metric/counter.hpp"
 #include "doctest/doctest.h"
@@ -21,6 +21,41 @@ TEST_CASE("test counter") {
 
   c.update({}, 0);
   CHECK(c.values().begin()->second.value == 0);
+
+  c.inc({"GET", "200"}, 1);
+  CHECK(c.values()[{"GET", "200"}].value == 1);
+  c.inc({"GET", "200"}, 2);
+  CHECK(c.values()[{"GET", "200"}].value == 3);
+
+  c.update({"GET", "200"}, 20);
+  CHECK(c.values()[{"GET", "200"}].value == 20);
+  c.reset();
+  CHECK(c.values()[{"GET", "200"}].value == 0);
+  CHECK(c.values().begin()->second.value == 0);
+}
+
+TEST_CASE("test guage") {
+  guage_t g("get_count", "get counter", {});
+  g.inc();
+  CHECK(g.values().begin()->second.value == 1);
+  g.inc();
+  CHECK(g.values().begin()->second.value == 2);
+  g.inc({}, 0);
+
+  g.dec();
+  CHECK(g.values().begin()->second.value == 1);
+  g.dec();
+  CHECK(g.values().begin()->second.value == 0);
+
+  g.inc({"GET", "200"}, 1);
+  CHECK(g.values()[{"GET", "200"}].value == 1);
+  g.inc({"GET", "200"}, 2);
+  CHECK(g.values()[{"GET", "200"}].value == 3);
+
+  g.dec({"GET", "200"}, 1);
+  CHECK(g.values()[{"GET", "200"}].value == 2);
+  g.dec({"GET", "200"}, 2);
+  CHECK(g.values()[{"GET", "200"}].value == 0);
 }
 
 DOCTEST_MSVC_SUPPRESS_WARNING_WITH_PUSH(4007)
