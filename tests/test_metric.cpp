4e77eee374845ebde18b711d7a8f89d7041eac33
qicosmos <qicosmos@163.com>
2024-05-06 11:37:35 +0800

    format

--- tests/test_metric.cpp
+++ tests/test_metric.cpp
@@ -1,26 +1,26 @@
 #include <stdexcept>
 #define DOCTEST_CONFIG_IMPLEMENT
-#include "doctest/doctest.h"
-
 #include "cinatra/metric/counter.hpp"
+#include "doctest/doctest.h"
 using namespace cinatra;
 
 TEST_CASE("test counter") {
-    counter_t c("get_count", "get counter", {});
-    c.inc();
-    CHECK(c.values().begin()->second.value==1);
-    c.inc();
-    CHECK(c.values().begin()->second.value==2);
-    c.inc({}, 0);
-    CHECK(c.values().begin()->second.value==2);
+  counter_t c("get_count", "get counter", {});
+  c.inc();
+  CHECK(c.values().begin()->second.value == 1);
+  c.inc();
+  CHECK(c.values().begin()->second.value == 2);
+  c.inc({}, 0);
 
-    CHECK_THROWS_AS(c.inc({}, -2), std::invalid_argument);
+  CHECK(c.values().begin()->second.value == 2);
 
-    c.update({}, 10);
-    CHECK(c.values().begin()->second.value==10);
+  CHECK_THROWS_AS(c.inc({}, -2), std::invalid_argument);
 
-    c.update({}, 0);
-    CHECK(c.values().begin()->second.value==0);
+  c.update({}, 10);
+  CHECK(c.values().begin()->second.value == 10);
+
+  c.update({}, 0);
+  CHECK(c.values().begin()->second.value == 0);
 }
 
 DOCTEST_MSVC_SUPPRESS_WARNING_WITH_PUSH(4007)
