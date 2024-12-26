3dfbd3ec11034fe9c3fb388d32faca66403d5f5c
qicosmos <qicosmos@163.com>
2024-12-26 14:01:00 +0800

    0.9.5

--- include/cinatra/version.hpp
+++ include/cinatra/version.hpp
@@ -5,4 +5,4 @@
 // CINATRA_VERSION % 100 is the sub-minor version
 // CINATRA_VERSION / 100 % 1000 is the minor version
 // CINATRA_VERSION / 100000 is the major version
-#define CINATRA_VERSION 902  // 0.9.2
+#define CINATRA_VERSION 905  // 0.9.5
