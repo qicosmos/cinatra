9f08143c28b924b8b668928ad2c0f060b76f23f6
osabc <e@osabc.com>
2025-04-28 23:49:39 +0800

    fix: append port to Host header for non-default ports

--- include/cinatra/coro_http_client.hpp
+++ include/cinatra/coro_http_client.hpp
@@ -1548,7 +1548,11 @@ class coro_http_client : public std::enable_shared_from_this<coro_http_client> {
     }
     else {
       if (req_headers_.find("Host") == req_headers_.end()) {
-        req_str.append(" HTTP/1.1\r\nHost:").append(u.host).append("\r\n");
+        req_str.append(" HTTP/1.1\r\nHost:").append(u.host);
+        if ((u.is_ssl && u.port.compare("443") != 0) ||
+            (!u.is_ssl && u.port.compare("80") != 0))
+          req_str.append(":").append(u.port);
+        req_str.append("\r\n");
       }
       else {
         req_str.append(" HTTP/1.1\r\n");
