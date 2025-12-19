6a00a8b4777098b52409dd60ba82a5bc4a0640e3
qicosmos <qicosmos@163.com>
2025-12-19 11:06:46 +0800

    Base64 decode (#722)

--- include/cinatra/utils.hpp
+++ include/cinatra/utils.hpp
@@ -209,6 +209,67 @@ inline std::string base64_encode(const std::string &str) {
   return ret;
 }
 
+inline bool is_base64(unsigned char c) {
+  return (isalnum(c) || (c == '+') || (c == '/') || (c == '-') || (c == '_'));
+}
+
+inline std::string base64_decode(const std::string &encoded_string) {
+  int in_len = encoded_string.size();
+  int i = 0;
+  int j = 0;
+  int in_ = 0;
+  unsigned char char_array_4[4], char_array_3[3];
+  std::string ret;
+
+  while (in_len-- && (encoded_string[in_] != '=') &&
+         is_base64(encoded_string[in_])) {
+    unsigned char current_char = encoded_string[in_++];
+
+    if (current_char == '-')
+      current_char = '+';
+    if (current_char == '_')
+      current_char = '/';
+    // -----------------------
+
+    char_array_4[i++] = current_char;
+    if (i == 4) {
+      for (i = 0; i < 4; i++)
+        char_array_4[i] = base64_chars.find(char_array_4[i]);
+
+      char_array_3[0] =
+          (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+      char_array_3[1] =
+          ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+      char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+
+      for (i = 0; (i < 3); i++) ret += char_array_3[i];
+      i = 0;
+    }
+  }
+
+  if (i) {
+    for (j = i; j < 4; j++) char_array_4[j] = 0;
+
+    for (j = 0; j < 4; j++) {
+      unsigned char c = char_array_4[j];
+      if (c == '-')
+        c = '+';
+      if (c == '_')
+        c = '/';
+      char_array_4[j] = base64_chars.find(c);
+    }
+
+    char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+    char_array_3[1] =
+        ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+    char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+
+    for (j = 0; (j < i - 1); j++) ret += char_array_3[j];
+  }
+
+  return ret;
+}
+
 // from h2o
 // inline const char *MAP =
 //    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
