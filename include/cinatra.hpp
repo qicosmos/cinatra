80194ed8db136cb58afdf1d3d8b5af5af7640bb2
qicosmos <qicosmos@163.com>
2026-01-02 20:23:48 +0800

    compile

--- include/cinatra.hpp
+++ include/cinatra.hpp
@@ -7,6 +7,6 @@
 
 #include "cinatra/coro_http_client.hpp"
 #include "cinatra/coro_http_server.hpp"
-#include "cinatra/smtp_client.hpp"
+// #include "cinatra/smtp_client.hpp"
 
 #endif  // CINATRA_CINATRA_HPP
